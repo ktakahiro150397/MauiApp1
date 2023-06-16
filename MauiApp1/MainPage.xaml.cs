feb346f5e1cf8080837b7770027ddddc533468c9
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2023-06-16 18:28:31 +0900

    カレンダー表示(デザイン崩れ中)

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,24 +1,18 @@
-﻿namespace MauiApp1;
+﻿using MauiApp1.ViewModels;
+
+namespace MauiApp1;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
 
+	MainPageViewModel vm;
+	
 	public MainPage()
 	{
 		InitializeComponent();
-	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+		vm = new MainPageViewModel();
+		this.BindingContext = vm;
+    }
 }
-
---
+++ MauiApp1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XCalendar.Core.Models;
+
+namespace MauiApp1.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public Calendar<CalendarDay> MyCalendar { get; set; } = new Calendar<CalendarDay>();
+
+            
+        public MainPageViewModel()
+        {
+        }
+
+    }
+}
